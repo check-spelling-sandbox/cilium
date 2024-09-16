fdb97d667d05bfdf268a167a057057781bcdccdc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-15 21:15:07 -0400

    spelling: process
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/bpf/events.go
+++ pkg/bpf/events.go
@@ -165,7 +165,7 @@ func (h *Handle) isFull() bool {
 // eventsBuffer.
 //
 // To prevent blocking bpf.Map operations, subscribed events are buffered per client handle.
-// How fast subscribers will need to proceess events will depend on the event throughput.
+// How fast subscribers will need to process events will depend on the event throughput.
 // In this case, our throughput will be expected to be not above 100 events a second.
 // Therefore the consumer will have 10ms to process each event. The channel is also
 // given a constant buffer size in the case where events arrive at once (i.e. all 100 events
