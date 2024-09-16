5545fdd15ceaf6f986bebef6fa43adf1f87b4541
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-15 22:06:07 -0400

    spelling: variety
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/loadbalancer/experimental/doc.go
+++ pkg/loadbalancer/experimental/doc.go
@@ -3,6 +3,6 @@
 
 // Package experimental contains a new experimental load-balancer control-plane based
 // on StateDB. It aims to simplify the control-plane down to fewer layers and open up
-// the ability to modify and observe frontends and backends for variaty of use-cases
+// the ability to modify and observe frontends and backends for variety of use-cases
 // without the need to special-case them.
 package experimental
