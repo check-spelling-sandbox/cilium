9d47c892348908b2434353d1c80b78272a14c001
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-09-15 21:02:47 -0400

    spelling: payload
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- bpf/tests/ipsec_from_host_generic.h
+++ bpf/tests/ipsec_from_host_generic.h
@@ -181,7 +181,7 @@ int ipv4_ipsec_from_host_check(__maybe_unused const struct __ctx_buff *ctx)
 
 	payload = (void *)l4 + sizeof(struct ip_esp_hdr);
 	if ((void *)payload + sizeof(default_data) > data_end)
-		test_fatal("paylaod out of bounds\n");
+		test_fatal("payload out of bounds\n");
 
 	if (memcmp(payload, default_data, sizeof(default_data)) != 0)
 		test_fatal("tcp payload was changed");
@@ -302,7 +302,7 @@ int ipv6_ipsec_from_host_check(__maybe_unused const struct __ctx_buff *ctx)
 
 	payload = (void *)l4 + sizeof(struct ip_esp_hdr);
 	if ((void *)payload + sizeof(default_data) > data_end)
-		test_fatal("paylaod out of bounds\n");
+		test_fatal("payload out of bounds\n");
 
 	if (memcmp(payload, default_data, sizeof(default_data)) != 0)
 		test_fatal("tcp payload was changed");
